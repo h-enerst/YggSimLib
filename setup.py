ffd6108b261a1a19894f77a37a0e0d58fd99598d
h-enerst <enerst@gmail.com>
2024-10-21 21:49:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="YggSimLib",
+  version="0.1",
+  author="Håkon Enerstvedt",  
+  description="A library for interfacing with the kspice API for the Yggdrasil project",
+  long_description="open("README.md").read(),
+  long_description_content_type="text/markdown",
+  packages=find_packages(),
+  install_requires=[
+    "tkinter",
+    "networkx"
+    ]
+  python_requires='>=3.12'
+)
 
