8418d7f5608571423f4a8831dc92f0a53dfd92b5
Håkon Enerstvedt <hakon.enerstvedt@akerbp.com>
2024-10-22 09:55:51 +0200

    Restructured package

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
   version="0.1",
   author="Håkon Enerstvedt",  
   description="A library for interfacing with the kspice API for the Yggdrasil project",
-  long_description="open("README.md").read(),
+  long_description=open('README.md').read(),
   long_description_content_type="text/markdown",
   packages=find_packages(),
   install_requires=[
-    "tkinter",
+    "tk",
     "networkx"
-    ]
+    ],
   python_requires='>=3.12'
 )
 
